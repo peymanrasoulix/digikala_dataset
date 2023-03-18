224a3a3c38fd05465adc972d24bb4ec2b569aa23
Peyman <61409421+peymanrasoulix@users.noreply.github.com>
2023-03-18 23:44:48 +0330

    Update DigikalaContext.cs

--- Digikala1/Model/DigikalaContext.cs
+++ Digikala1/Model/DigikalaContext.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Digikala1.Model
 {
     class DigikalaContext
     {
         private Digikala digikala;
         private string line;
-        public List<Digikala> digikalas  { get; }
+        public List<Digikala> digikalas { get; }
         public DigikalaContext(string address)
         {
 
@@ -27,7 +21,7 @@ namespace Digikala1.Model
                     digikala.ID_Order = Convert.ToInt32(line.Split(",")[0]);
                     digikala.ID_Customer = Convert.ToInt32(line.Split(",")[1]);
                     digikala.ID_Item = Convert.ToInt32(line.Split(",")[2]);
-                    digikala.DateTime_CartFinalize = (line.Split(",")[3]).ToString();
+                    digikala.DateTime_CartFinalize = Convert.ToDateTime(line.Split(",")[3]);
                     digikala.Amount_Gross_Order = Convert.ToInt32(line.Split(",")[4].Replace(".0", ""));
                     digikala.city_name_fa = line.Split(",")[5];
 
@@ -37,3 +31,4 @@ namespace Digikala1.Model
         }
     }
 }
+
