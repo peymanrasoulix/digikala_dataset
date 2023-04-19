f502046e33c5216991efdea8fbab84024c120a37
Peyman <61409421+peymanrasoulix@users.noreply.github.com>
2023-04-19 17:18:14 +0330

    Update Program.cs

--- Digikala1/Program.cs
+++ Digikala1/Program.cs
@@ -10,18 +10,16 @@ lockkeyboard yn = new lockkeyboard();
 string q;
 do
 {
-    string address = @"F:\digikala\orders.csv";
+    string address = @"C:\orders.csv";
 
     DigikalaContext context = new DigikalaContext(address);
-
     DigikalaOperation op = new DigikalaOperation(context.digikalas);
-
     lockkeyboard lk = new lockkeyboard();
 
 
-    Console.WriteLine("WHAT DO YOU NEED? \n " + " PRICE BY ORDER (PRESS O) \n " +
-                      "TOTAL PRICE BY CUSTOMER (PRESS C) \n TOTAL PRICE BY ITEM PRESS(I)" +
-                      "\n TOTAL PRICE BY TIME (PRESS T) \n TOTAL PRICE BY CITY (PRESS Y)");
+    Console.WriteLine("WHAT DO YOU NEED? \n " + " PRICE BY ORDER (O) \n " +
+                      "TOTAL PRICE BY CUSTOMER (C) \n TOTAL PRICE BY ITEM (I)" +
+                      "\n TOTAL PRICE BY TIME (T) \n TOTAL PRICE BY CITY (Y)");
     string x = lk.menulocker();
 
 
@@ -30,31 +28,24 @@ do
         case "o" or "O":
             Console.WriteLine("ENTER ID_ORDER");
             int o = lk.keyboardnumberlocker();
-
             Console.WriteLine(op.priceSalesByorder(o));
-
             break;
 
         case "c" or "C":
             Console.WriteLine("ENTER ID_CUSTOMER");
             int c = lk.keyboardnumberlocker();
-
             Console.WriteLine(op.SumSalesBycustomer(c));
-
             break;
 
 
         case "i" or "I":
             Console.WriteLine("ENTER ID_ITEM");
             int i = lk.keyboardnumberlocker();
-
             Console.WriteLine(op.sumSalesByitem(i));
-
             break;
 
-
         case "t" or "T":
-            Console.WriteLine("ENTER DATE");
+            Console.WriteLine("enter data");
             int t = lk.keyboardnumberlocker();
 
             Console.WriteLine(op.SumSalesByYear(t));
@@ -63,7 +54,7 @@ do
 
 
         case "y" or "Y":
-            Console.WriteLine("ENTER CITY (FARSI)");
+            Console.WriteLine("ENTER Your City (Persian)");
             int y = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine(op.SumSalescity_name(y));
@@ -74,12 +65,12 @@ do
             throw new Exception("INVALID CHARACTER");
 
     }
-    Console.WriteLine("DO YOU WANT TRY AGAIN ? (Y) OR (N)");
+    Console.WriteLine("DO YOU WANT TRY AGAIN ? (Y OR N)");
     q = lk.keyboardYESORNOlocker();
 
 } while (q.ToLower() == "y");
 
-Console.WriteLine("DO YOU WANT TO ORGANIZ DATASET BY CITY ? (Y) OR (N)");
+Console.WriteLine("DO YOU WANT TO ORGANIZ DATASET BY CITY ? (Y OR N)");
 string g = yn.keyboardYESORNOlocker();
 
 if (g.ToLower() != "n")
